53e45b69e027e8d654c4ea1027d49a8a4bfb3295
Aleexfdez <alexfernandez567@gmail.com>
2023-10-04 09:45:11 +0200

    fin

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,1 +1,11 @@
-var arrayGrande=new Array();
+const size = 50;
+const value = 0;
+var arrayGrande = new Array(size);
+
+for (var i = 0; i < arrayGrande.length; i++) {
+    arrayGrande[i] = value;
+}
+console.log(arrayGrande);
+
+var arrayPro = new Array(size).fill(value);
+console.log(arrayGrande);
